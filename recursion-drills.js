648fa03f5ef4fa4ad7b891c8b7a152991676b95a
Sara Mayberry <saraamayberry@yahoo.com>
2019-07-08 22:31:24 -0600

    initial

---
+++ recursion-drills.js
@@ -0,0 +1,38 @@
+// Counting Sheep drill 1 
+// ? I'm not sure what they are asking of in this 
+const sheepCount = function(sheep) {
+    //base case
+    if (sheep.length === 1) {
+        return sheep[0];
+    }
+    //General case 
+    return sheep[0] + sheepCount(sheep.slice(1))
+}
+
+
+//2. Power Calculator 
+
+function powerCalculator(int1, int2) {
+    return Math.pow(int1, int2)
+}
+
+
+//3. Reverse string 
+function reverseString(str) {
+    // use split to return a new array
+    let splitString = str.split('');
+
+    //use reverse method to reverse the new created array
+    let reverseArray = splitString.reverse();
+
+    //use join method to join the elements into a string
+    let joinArray = reverseArray.join('')
+
+    //return the reverse string
+    return joinArray;
+}
+
+//4. nth Triangular Number .... not a clue how to solve this.
+
+
+//5. String splitter 
