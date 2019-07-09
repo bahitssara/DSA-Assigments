f2e430bf19075d88621355ddee9f39b14e7cb457
Sara Mayberry <saraamayberry@yahoo.com>
2019-07-09 13:25:21 -0600

    big-o-assignment

---
+++ big-o-assignment.js
@@ -0,0 +1,157 @@
+/*1. What is the Big O for this?
+1) Determine the Big O for the following algorithm: You are sitting in a room with 15 people. You want to find a playmate for your dog, preferably of the same breed. So you want to know if anyone out of the 15 people have the same breed as your dog. You stand up and yell out, who here has a golden retriever and would like to be a playdate for my golden. Someone yells - "I do, be happy to bring him over"
+- O(1)-Constant time, because no matter how many people are in the room, they will all hear me and the response time will remain the same. 
+
+2) Determine the Big O for the following algorithm: You are sitting in a room with 15 people. You want to find a playmate for your dog who is of the same breed. So you want to know if anyone out of the 15 people have the same breed as your dog. You start with the first person and ask him if he has a golden retriever. He says no, then you ask the next person, and the next, and the next until you find someone who has a golden or there is no one else to ask.
+O(n)-Linear time, because I will take the same amount of time to speak to each person individually which will proportionately add time with each person asked no matter how many are in the room. 
+*/
+
+
+// 2. Even or odd
+function isEven(value) {
+    if (value % 2 == 0) {
+        return true;
+    }
+    else
+        return false;
+}
+// Linear OR Constant was my inital thought, see now it is constant. Confirm why/ *** Ask Marius. 
+
+
+
+// 3. Are you here?
+function areYouHere(arr1, arr2) {
+    for (let i = 0; i < arr1.length; i++) {
+        const el1 = arr1[i];
+        for (let j = 0; j < arr2.length; j++) {
+            const el2 = arr2[j];
+            if (el1 === el2) return true;
+        }
+    }
+    return false;
+}
+// Polynomial time? O(n^k), this function will increase depending on how many loops it has. 
+
+// 4. Doubler
+// What is the Big O of the following algorithm? Explain your answer
+
+function doubleArrayValues(array) {
+    for (let i = 0; i < array.length; i++) {
+        array[i] *= 2;
+    }
+    return array;
+}
+// 0(n), is this because we are passing an array through one method, and only expecting one output? 
+
+
+// 5. Naive search
+function naiveSearch(array, item) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === item) {
+            return i;
+        }
+    }
+}
+//Linear 0(n)? Because we need to check each item in the array to determine if it is === to the expected item? Bigger the data set each will take the same time? ORRRRR Logarithmic time? 
+
+// 6. Creating pairs:
+// What is the Big O of the following algorithm? Explain your answer
+function createPairs(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for(let j = i + 1; j < arr.length; j++) {
+            console.log(arr[i] + ", " +  arr[j] );
+        }
+    }
+}
+// Not sure on this one** 
+
+
+// 6. Compute the sequence
+function compute(num) {
+    let result = [];
+    for (let i = 1; i <= num; i++) {
+
+        if (i === 1) {
+            result.push(0);
+        }
+        else if (i == 2) {
+            result.push(1);
+        }
+        else {
+            result.push(result[i - 2] + result[i - 3]);
+        }
+    }
+    return result;
+}
+//ANSWER HERE
+
+// 7. Compute the sequence
+function compute(num) {
+    let result = [];
+    for (let i = 1; i <= num; i++) {
+
+        if (i === 1) {
+            result.push(0);
+        }
+        else if (i == 2) {
+            result.push(1);
+        }
+        else {
+            result.push(result[i - 2] + result[i - 3]);
+        }
+    }
+    return result;
+}
+//Answer Here
+
+//8. An efficient search
+// In this example, we return to the problem of searching using a more sophisticated approach than in naive search, above. Assume that the input array is always sorted. What is the Big O of the following algorithm? Explain your answer
+function efficientSearch(array, item) {
+    let minIndex = 0;
+    let maxIndex = array.length - 1;
+    let currentIndex;
+    let currentElement;
+
+    while (minIndex <= maxIndex) {
+        currentIndex = Math.floor((minIndex + maxIndex) / 2);
+        currentElement = array[currentIndex];
+
+        if (currentElement < item) {
+            minIndex = currentIndex + 1;
+        }
+        else if (currentElement > item) {
+            maxIndex = currentIndex - 1;
+        }
+        else {
+            return currentIndex;
+        }
+    }
+    return -1;
+}
+//ANSWER HERE 
+
+//9. Random element
+function findRandomElement(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+//ANSWER HERE
+
+// 10. What Am I?
+// What does the following algorithm do? What is the Big O of the following algorithm? Explain your answer
+function isWhat(n) {
+    if (n < 2 || n % 1 != 0) {
+        return false;
+    }
+    for (let i = 2; i < n; ++i) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+//ANSWER HERE
+
+//11-14. Ask Marius to go over it with you
+
+
+
+
+
--- recursion-drills.js
+++ recursion-drills.js
@@ -1,4 +1,4 @@
-// Counting Sheep drill 1 
+//1. Counting Sheep drill 
 // ? I'm not sure what they are asking of in this 
 const sheepCount = function(sheep) {
     //base case
@@ -19,23 +19,145 @@ function powerCalculator(int1, int2) {
     return Math.pow(int1, int2)
 }
 
+//3. Reverse string
+/*Reverse String: Take a string as input, reverse the string, and return the new string. */
 
-//3. Reverse string 
-function reverseString(str) {
-    // use split to return a new array
-    let splitString = str.split('');
+let reverse = (string, string2) => {
+    string2 = !string2 ? '' : string2;
+  
+    if (string.length == 0) return string2;
+  
+    string2 = string2 + string[string.length - 1];
+  
+    string = string.substring(0, string.length - 1);
+  
+    return reverse(string, string2);
+  }
+  
+  console.log(reverse('t'));
 
-    //use reverse method to reverse the new created array
-    let reverseArray = splitString.reverse();
-
-    //use join method to join the elements into a string
-    let joinArray = reverseArray.join('')
-
-    //return the reverse string
-    return joinArray;
-}
+  
+  // alternate solution
+  
+  let reverse = (str, str2) => {
+      if (!str2) {
+          str = str.split('');
+          str2 = [];
+      }
+  
+      if (str.length === 0) return str2.join('');
+  
+      str2.unshift(str[0]);
+      str.shift();
+  
+      return reverse(str, str2);
+  }
+  
+  console.log(reverse('string'));
 
 //4. nth Triangular Number .... not a clue how to solve this.
+/* Exercise 4: Calculates the nth triangular number.
+A triangular number counts the objects that can form an equilateral triangle. The nth triangular number is the number of dots composing a triangle with n dots on a side, and is equal to the sum of the n natural numbers from 1 to n. This is the Triangular Number Sequence: 1, 3, 6, 10, 15, 21, 28, 36, 45.
+*/
 
+let nthTriNum = (n, t) => {
+    t = !t ? 0 : t;
+
+    if (n === 0) {
+        return 'T = ' + t;
+    }
+
+    t = t + n;
+    n = n - 1;
+
+    return nthTriNum(n, t);
+}
+
+console.log(nthTriNum(6));
 
 //5. String splitter 
+/* Exercise 5:
+Write a recursive function that split a string based on a separator (similar to String.prototype.split).
+*/
+
+let separator = (str, sep, arr, str2) => {
+  arr = arr || [];
+  str2 = str2 || '';
+
+  // when no separator is passed
+  if (!sep && sep !== '') return arr;
+
+  // when no string is passed
+  if (!str && !str2) return arr;
+
+  // when str is finished but str2 still has a string in it
+  if (!str && str2) {
+    arr.push(str2);
+    return arr;
+  }
+
+  // adds next char to str2 when its not equal to sep
+  if (str.substring(0, sep.length) !== sep) {
+    str2 = str2.concat(str[0]);
+  }
+  // when it matches sep or when sep is an empty string
+  if (str.substring(1, sep.length + 1) === sep || sep === '') {
+    arr.push(str2);
+    str2 = '';
+  }
+
+  // chooses next str to pass to recursion
+  // if str matches sep it will choose substring beyond the sep
+  if (str.substring(0, sep.length) === sep) {
+    str = str.substring (sep.length, str.length);
+  } else { // else it will do the next char to the end of string
+    str = str.substring(1, str.length);
+  }
+  // calls recursively
+  return separator(str, sep, arr, str2);
+}
+
+//6. Fibonacci
+/* Exercise 6: Calculates the nth triangular number.
+Write a recursive function that prints the Fibonacci sequence of a given number. The Fibonacci sequence is a series of numbers in which each number is the sum of the 2 preceding numbers. For example, the 7th Fibonacci number in a Fibonacci sequence is 13. The sequence looks as follows: 1, 1, 2, 3, 5, 8, 13.
+*/
+
+function fibonacci_series(n) {
+    if (n === 1) {
+        return [0, 1];
+    } else {
+        let s = fibonacci_series(n - 1);
+        s.push(s[s.length - 1] + s[s.length - 2]);
+        return s;
+    }
+};
+
+console.log(fibonacci_series(8));
+
+//7. Factorial 
+// a recursion is a function calling itself
+/*
+Write a recursive function that finds the factorial of a given number. The factorial of a number can be found by multiplying that number by each number between itself and 1. For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
+*/
+function factorial(n) {
+
+    // terminate the recursion once we hit zero
+    if (n === 0) {
+        console.log('the recursion has reached ' + 0);
+        return 1;
+    }
+
+    // otherwise keep calling the function recursively
+    else {
+        console.log("inside the recursion n = " + n)
+        // the function calls itself
+        return factorial(n - 1) * n;
+    }
+}
+
+console.log(factorial(5));
+
+//10. Anagrams 
+
+
+console.log(separator('monkey is happy', ' '));
